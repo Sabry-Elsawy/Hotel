c4477c7d4e8bf4555c8941e6f7da0db5a2a02336
Sabry Elsawy <elsawysabry16@gmail.com>
2025-03-08 18:01:11 +0200

    handle logic in most popular ads page

--- src/app/landing-page/home/home.component.ts
+++ src/app/landing-page/home/home.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AdsUserService } from '../services/ads-service/ads-user.service';
+import { IAds } from '../../core/model/ads';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 capacity:number=0;
-
+tableData: any;
+tableUserAds: IAds[] = [];
+constructor(private _AdsUserService:AdsUserService) { }
+ngOnInit(): void {
+ this.getAllAds(this.tableUserAds)
+ 
+}
 increasementCapacity(){
   this.capacity++;  
 }
@@ -16,6 +24,21 @@ decreasementCapacity(){
      this.capacity--;
     
    }
+}
+getAllAds(data:any){
+  this._AdsUserService.getAllAds(data).subscribe({
+    next: (response) => {
+      this.tableData = response;
+      this.tableUserAds = response.data.ads.slice(0,5);
+    },
+    error: (err) => {
+      console.log(err);
+    },
+    complete: () => {
+      console.log('complete');
+    }
+  });
+
 }
 
 }
